1e9a279feecf2d60d45cf4322975c91d4369a5ad
mBleakley347 <mbleakley347@gmail.com>
2020-10-30 14:44:17 +1100

    Initial commit

---
+++ Assets/Scripts/SCR_FollowTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SCR_FollowTest : MonoBehaviour 
+{
+    public GameObject target;
+    public float time;
+
+    public bool found;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (found) transform.position = Vector3.MoveTowards(this.transform.position, target.transform.position, time);
+    }
+
+    public void OnFound()
+    {
+        found = true;
+    }
+
+    public void OnLost()
+    {
+        found = false;
+    }
+}
